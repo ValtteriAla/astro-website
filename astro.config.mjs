0e0de250dd73bbd12463053bcf827e1553c86751
ValtteriAla <valtteri.m.alastalo@edu.riveria.fi>
2024-04-10 12:23:55 +0300

    Added grid to page | Added Base layout | Added Aliases

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,14 @@ import sitemap from '@astrojs/sitemap';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
-
 import vue from "@astrojs/vue";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), react(), tailwind(), svelte(), vue()]
+  integrations: [mdx(), sitemap(), react(), tailwind(), svelte(), vue()],
+  output: "server",
+  adapter: vercel()
 });
